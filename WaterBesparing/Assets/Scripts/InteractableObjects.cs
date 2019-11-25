815875d7d67d3f11f867c4e4522a5ee9ac1d5de9
bunnykilll <524444@student.glu.nl>
2019-11-25 11:53:59 +0100

    Test Scene Rico

--- WaterBesparing/Assets/Scripts/InteractableObjects.cs
+++ WaterBesparing/Assets/Scripts/InteractableObjects.cs
@@ -4,8 +4,38 @@ using UnityEngine;
 using UnityEngine.UI; 
 public class InteractableObjects : MonoBehaviour
 {
-    
 
+    public Button WC;
+    public Button Keuze1;
+    public Button Keuze2;
+    public Button Keuze3;
+    public Button Keuze4;
+
+
+    void OnEnable()
+    {
+        WC.gameObject.SetActive(true);
+        Keuze1.gameObject.SetActive(false);
+        Keuze2.gameObject.SetActive(false);
+        Keuze3.gameObject.SetActive(false);
+        Keuze4.gameObject.SetActive(false);
+
+    }
+
+    private void disableAllButtons()
+    {
+        WC.gameObject.SetActive(false);
+        Keuze1.gameObject.SetActive(false);
+        Keuze2.gameObject.SetActive(false);
+        Keuze3.gameObject.SetActive(false);
+        Keuze4.gameObject.SetActive(false);
+    }
+
+    public void EnableButton(Button _toEnable)
+    {
+        disableAllButtons();
+        _toEnable.gameObject.SetActive(true);
+    }
 
     void Start()
     {
