f50823a9accc1e5de0cf1ae0aa82896ac51d47fb
Thomas Sluijs <529181@student.glu.nl>
2020-01-13 11:32:06 +0100

    Movement fixes

---
+++ WaterBesparing/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public List<GameObject> RoomPositions;
+
+    private PlayerScript Player;
+
+    RoomPos PlayerEnum;
+
+    public int rooms;
+
+    bool LeftRotation;
+
+    //PlayerScript player = new PlayerScript();
+
+
+    void Start()
+    {
+        //Player = GameObject.Find("Player").GetComponent<PlayerScript>();
+
+    }
+    void Update()
+    {
+        if (PlayerEnum == RoomPos.BedroomEnum)
+        {
+            rooms = 0;
+            Camera.main.transform.position = RoomPositions[rooms].transform.position;
+            Camera.main.transform.rotation = RoomPositions[rooms].transform.rotation;
+        }
+        else if (PlayerEnum == RoomPos.BathroomEnum)
+        {
+            print("but now it doent");
+            rooms = 1;
+            Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, RoomPositions[rooms].transform.position, 0.05f);
+            Camera.main.transform.rotation = RoomPositions[rooms].transform.rotation;
+        }
+        else if (PlayerEnum == RoomPos.KitchenEnum)
+        {
+            print("but now it doent");
+            rooms = 2;
+            Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, RoomPositions[rooms].transform.position, 0.1f);
+            Camera.main.transform.rotation = RoomPositions[rooms].transform.rotation;
+        }
+
+    }
+
+}
--- WaterBesparing/Assets/Scripts/PlayerScript.cs
+++ WaterBesparing/Assets/Scripts/PlayerScript.cs
@@ -1,6 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
+public enum RoomPos
+{
+    BedroomEnum,
+    BathroomEnum,
+    KitchenEnum,
+    Outside,
+    Garage
+}
 
 public class PlayerScript : MonoBehaviour
 {
@@ -8,12 +18,18 @@ public class PlayerScript : MonoBehaviour
     public int Destination = 0;
     public LayerMask touchInputMask;
 
-
-    public float minDist;
-    public float speed;
-    public float dist;
+    RoomPos Movement = RoomPos.BedroomEnum;
 
     public bool active = true;
+
+    public GameObject TestPoint;
+
+    public NavMeshAgent agent;
+
+    public void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
 
     void Update()
     {
@@ -35,71 +51,42 @@ public class PlayerScript : MonoBehaviour
             }
         }
 
-        dist = Vector3.Distance(gameObject.transform.position, waypoints[Destination].transform.position);
-
-        if (active)
+        switch (Movement)
         {
+            case RoomPos.BedroomEnum:
+                if (Input.GetKeyDown(KeyCode.H))
+                {
+                    agent.destination = waypoints[1].transform.position;
+                    Movement = RoomPos.BathroomEnum;
+                }
+                break;
+            case RoomPos.BathroomEnum:
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                    agent.destination = waypoints[2].transform.position;
+                    Movement = RoomPos.BathroomEnum;
+                }
+                break;
+            case RoomPos.KitchenEnum:
+                if (Input.GetKeyDown(KeyCode.A))
+                {
 
-            if (dist < minDist)
-            {
-                // Hallway Upstairs
-                if (Destination == 1)
-                {
-                    Destination = 2;
                 }
-                else if (Destination == 2)
+                break;
+            case RoomPos.Outside:
+                if (Input.GetKeyDown(KeyCode.A))
                 {
-                    Destination = 3;
-                }
-                else if (Destination == 3)
-                {
-                    Destination = 4;
-                }
-                //stairs
-                if (Destination == 5)
-                {
-                    Destination = 6;
-                }
-                else if (Destination == 6)
-                {
-                    Destination = 7;
-                }
-                else if (Destination == 7)
-                {
-                    Destination = 8;
-                }
-                else if (Destination == 8)
-                {
-                    Destination = 9;
-                }
-                else if (Destination == 9)
-                {
-                    Destination = 10;
-                }
-                else if (Destination == 10)
-                {
-                    Destination = 11;
-                }
 
-            }
-
-            if (dist > minDist)
-            {
-                Move();
-            }
-            else
-            {
-                if (Input.GetKeyDown(KeyCode.Alpha1))
-                {
-                    Destination++;
                 }
-            }
+                break;
+            case RoomPos.Garage:
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+
+                }
+                break;
         }
-    }
 
-    private void Move()
-    {
-        gameObject.transform.LookAt(waypoints[Destination].transform.position);
-        gameObject.transform.position += gameObject.transform.forward * speed * Time.deltaTime;
     }
 }
+
