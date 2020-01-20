611c007d72e941fa8bca95637553dc407ded835f
bunnykilll <524444@student.glu.nl>
2020-01-20 12:23:06 +0100

    UImanager changes
    paar dingentjes

--- WaterBesparing/Assets/Scripts/Managers/UImanager.cs
+++ WaterBesparing/Assets/Scripts/Managers/UImanager.cs
@@ -6,18 +6,72 @@ public class UImanager : MonoBehaviour
 {
 
 
-    [SerializeField] public GameObject KitchenKeuze_1; 
-    public bool kitchenActive; 
+    [SerializeField] public GameObject AfwasKeuze_1;
+    [SerializeField] public GameObject AfwasKeuze_2;
+    [SerializeField] public GameObject AfwasKeuze_3;
+
+    [SerializeField] public GameObject ToiletKeuze_1;
+    [SerializeField] public GameObject ToiletKeuze_2;
+    [SerializeField] public GameObject ToiletKeuze_3;
+
+    [SerializeField] public GameObject OpschonenKeuze_1;
+    [SerializeField] public GameObject OpschonenKeuze_2;
+    [SerializeField] public GameObject OpschonenKeuze_3;
+
+    [SerializeField] public GameObject WasmachineKeuze_1;
+    [SerializeField] public GameObject WasmachineKeuze_2;
+    [SerializeField] public GameObject WasmachineKeuze_3;
+
+    [SerializeField] public GameObject PlantenWaterenKeuze_1;
+    [SerializeField] public GameObject PlantenWaterenKeuze_2;
+    [SerializeField] public GameObject PlantenWaterenKeuze_3;
+
+    public bool AfwasActive;
+    public bool ToiletActive;
+    public bool PlantenWaterenActive;
+    public bool WasmachineActive;
+    public bool OpschonenActive;
 
     void Start()
     {
-        DontDestroyOnLoad(this); 
+        DontDestroyOnLoad(this);
     }
     void Update()
     {
-        if(kitchenActive)
+        if (AfwasActive)
         {
-            KitchenKeuze_1.gameObject.SetActive(true); 
+            AfwasKeuze_1.SetActive(true);
+            AfwasKeuze_2.SetActive(true);
+            AfwasKeuze_3.SetActive(true);
+        }
+
+        if (ToiletActive)
+        {
+            ToiletKeuze_1.SetActive(true);
+            ToiletKeuze_2.SetActive(true);
+            ToiletKeuze_3.SetActive(true);
+        }
+
+        if (PlantenWaterenActive)
+        {
+            PlantenWaterenKeuze_1.SetActive(true);
+            PlantenWaterenKeuze_2.SetActive(true);
+            PlantenWaterenKeuze_3.SetActive(true);
+        }
+
+        if (WasmachineActive)
+        {
+            WasmachineKeuze_1.SetActive(true);
+            WasmachineKeuze_2.SetActive(true);
+            WasmachineKeuze_3.SetActive(true);
+        }
+
+        if (OpschonenActive)
+        {
+            OpschonenKeuze_1.SetActive(true);
+            OpschonenKeuze_2.SetActive(true);
+            OpschonenKeuze_3.SetActive(true);
         }
     }
+
 }
